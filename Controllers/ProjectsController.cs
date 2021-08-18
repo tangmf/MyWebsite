3de8381dd31c115c3054d85b28fccd9fba2c6095
Tang Ming Feng <derpsnowtmf@gmail.com>
2021-08-18 21:21:43 +0800

    Added projects pages

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebsite.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Art()
+        {
+            return View();
+        }
+
+        public IActionResult Games()
+        {
+            return View();
+        }
+
+        public IActionResult Websites()
+        {
+            return View();
+        }
+
+        public IActionResult Others()
+        {
+            return View();
+        }
+    }
+}
